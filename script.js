a2dc1444210967f1a873e2d0b1b32b6688fdba75
impressiveaustin <47372004+impressiveaustin@users.noreply.github.com>
2021-05-13 04:29:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+  create_random_string()
+function create_random_string(){
+  var random_string = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'
+  for(var i, i = 0; i < characters.length; i++){
+    random_string += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+}
