4e9d78e34d33d02d58b4ed29a711d722586b7b0a
impressiveaustin <47372004+impressiveaustin@users.noreply.github.com>
2021-05-13 05:32:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('offertoro').src = generate_url(8)
 function generate_url(string_length){
   var random_string = '';
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'
-  for(var i, i = 0; i < string_length; i++){
+  for(var i = 0; i < string_length; i++){
     random_string += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return `https://www.offertoro.com/ifr/show/24707/${random_string}/12471`
