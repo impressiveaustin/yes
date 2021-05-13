3d46a8dabbe7ce61586dcad2b2befb7459d21b83
impressiveaustin <47372004+impressiveaustin@users.noreply.github.com>
2021-05-13 05:30:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.getElementById('code_string').innerHTML = create_random_string(8)
+document.getElementById('offertoro').src = generate_url(8)
 
-function create_random_string(string_length){
+function generate_url(string_length){
   var random_string = '';
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'
   for(var i, i = 0; i < string_length; i++){
     random_string += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  return random_string
+  return `https://www.offertoro.com/ifr/show/24707/${random_string}/12471`
 }
